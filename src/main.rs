f34730ffac8ae36889b1caf42879dce706b6bf82
Muhammet Furkan Er <furkan.er@siemens.com>
2022-06-17 14:25:59 +0300

    added info message

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use std::{thread, time};
+use std::{thread, time, io::{self, Write}};
 use winput::{Vk};
 
+use colored::Colorize;
+
 fn main() {
-    
+    let the_key = Vk::Tab;
+    let sleep_for_seconds = 180;
+
+    print!("{}",format!("\n\tSimulating {:?} every {} seconds.",the_key, sleep_for_seconds).red().bold());
+    io::stdout().flush().unwrap();
+
     loop{
-        let sleep_time = time::Duration::from_secs(180);
+        let sleep_time = time::Duration::from_secs(sleep_for_seconds);
         thread::sleep(sleep_time);
-        winput::send(Vk::Tab); 
+        winput::send(the_key); 
     }
 }
